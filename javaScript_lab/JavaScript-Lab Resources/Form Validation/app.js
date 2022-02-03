64414dc0dfc9f9b5f11ab60ffca86f463147a252
Jonothan <jonothan.x.castillo@gmail.com>
2022-02-02 20:21:50 -0600

    Commit rough draft of FORM VALIDATION javascript

--- javaScript_lab/JavaScript-Lab Resources/Form Validation/app.js
+++ javaScript_lab/JavaScript-Lab Resources/Form Validation/app.js
@@ -1,1 +1,36 @@
-console.log("js file connected to HTML");
+const inputList = document.getElementsByTagName("input");
+const errorText = document.getElementsByClassName("error-text");
+const errorExclamation = document.getElementsByClassName("input-error");
+const emailPlaceholder = document.getElementById("email");
+const emailError = document.getElementById("email-error");
+
+document.querySelector('#form-submit').addEventListener('click', onClick)
+
+function onClick(){
+
+    for(let i = 0; i < inputList.length; i++){
+
+        if(inputList[i].value != '' && i != 2 ){
+            errorText[i].style.visibility = 'hidden';
+            errorExclamation[i].style.visibility = 'hidden';
+        } 
+        else{
+            errorText[i].style.visibility = 'visible';
+            errorExclamation[i].style.visibility = 'visible';
+        }
+
+        if(i == 2 && inputList[i].value.toString().includes('@') && inputList[i].value.toString().includes('.')){
+            errorText[i].style.visibility = 'hidden';
+            errorExclamation[i].style.visibility = 'hidden';
+        }
+        else if(inputList[i].value != '' && i == 2 ){
+            emailPlaceholder.placholder = "email@example/com";
+            emailError.innerText = "Looks like this is not an email";
+            errorText[i].style.visibility = 'visible';
+            errorExclamation[i].style.visibility = 'visible';
+            inputList[i].value = '';
+        }
+        
+
+    }
+}
