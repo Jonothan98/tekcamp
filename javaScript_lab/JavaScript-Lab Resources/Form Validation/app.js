725fde30e4a617f7177a94c890f594dc3ca85d2a
Jonothan <jonothan.x.castillo@gmail.com>
2022-02-04 14:40:54 -0600

    commit finishing touches to Form Validation javaScript file

--- javaScript_lab/JavaScript-Lab Resources/Form Validation/app.js
+++ javaScript_lab/JavaScript-Lab Resources/Form Validation/app.js
@@ -1,7 +1,7 @@
 const inputList = document.getElementsByTagName("input");
 const errorText = document.getElementsByClassName("error-text");
 const errorExclamation = document.getElementsByClassName("input-error");
-const emailPlaceholder = document.getElementById("email");
+const email = document.getElementById("email");
 const emailError = document.getElementById("email-error");
 
 //Regex formula provided by: https://www.w3resource.com/javascript/form/email-validation.php
@@ -9,32 +9,38 @@ var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 document.querySelector('#form-submit').addEventListener('click', onClick)
 
+//OnClick funtion checks if inputs are empty then checks if email was submitted correctly
 function onClick(){
 
     for(let i = 0; i < inputList.length; i++){
 
-        if(inputList[i].value != '' && i != 2 ){
-            errorText[i].style.visibility = 'hidden';
-            errorExclamation[i].style.visibility = 'hidden';
+        if(inputList[i].value == '' ){
+            errorText[i].style.visibility = 'visible';
+            errorExclamation[i].style.visibility = 'visible';
+            inputList[i].placeholder = "";
+            inputList[i].classList.add("error");
         } 
         else{
-            errorText[i].style.visibility = 'visible';
-            errorExclamation[i].style.visibility = 'visible';
-        }
-
-        if(i == 2 && inputList[i].value.toString().match(mailformat)){
             errorText[i].style.visibility = 'hidden';
             errorExclamation[i].style.visibility = 'hidden';
-
+            inputList[i].classList.remove("error");
         }
-        else if( i == 2 && inputList[i].value != '' ){
-            emailPlaceholder.placeholder = "email@example/com";
-            emailError.innerText = "Looks like this is not an email";
-            errorText[i].style.visibility = 'visible';
-            errorExclamation[i].style.visibility = 'visible';
-            inputList[i].value = '';
-        }
-        
-
+    
     }
+
+    if(email.value !== "" && email.value.match(mailformat) == null){ 
+        emailError.innerText = "Looks like this is not an email";
+        emailError.style.visibility = 'visible';
+        document.querySelector("#email").previousElementSibling.style.visibility = 'visible';
+        document.querySelector("#email").classList.add("error");
+        document.querySelector("#email").classList.add("invalid-email");
+    
+    } else if(email.value.match(mailformat) !== null){
+        emailError.innerText = "";
+        emailError.style.visibility = 'hidden';
+        document.querySelector("#email").previousElementSibling.style.visibility = 'hidden';
+        document.querySelector("#email").classList.remove("error");
+        document.querySelector("#email").classList.remove("invalid-email");
+    }
+
 }
