92db9486689b033e8d2f57fe0a24f674aacda08b
Jonothan <jonothan.x.castillo@gmail.com>
2022-02-06 14:11:03 -0600

    added bold text toggle to Accordion JavaScript file

--- javaScript_lab/JavaScript-Lab Resources/Accordion/app.js
+++ javaScript_lab/JavaScript-Lab Resources/Accordion/app.js
@@ -5,23 +5,25 @@ faqList.addEventListener('click', onClick);
 function onClick(itemClicked){
 
     if(itemClicked.target.parentElement.classList.contains("faq-item") && itemClicked.target.classList.contains("question")){
-        toggleDropBox(itemClicked.target.parentElement.lastElementChild,itemClicked.target.firstElementChild);
+        toggleDropBox(itemClicked.target.parentElement.lastElementChild,itemClicked.target.firstElementChild, itemClicked.target);
 
     } else if(itemClicked.target.parentElement.parentElement.classList.contains("faq-item") && itemClicked.target.classList.contains("arrow-icon")){
-        toggleDropBox(itemClicked.target.parentElement.parentElement.lastElementChild, itemClicked.target);
+        toggleDropBox(itemClicked.target.parentElement.parentElement.lastElementChild, itemClicked.target, itemClicked.target.parentElement);
 
     }
     
 }
 
-function toggleDropBox(textToggle, imageToggle){
+function toggleDropBox(textToggle, imageToggle, boldToggle){
 
     if(textToggle.style.display === "block"){
         textToggle.style.display = "none";
         imageToggle.style.transform = "rotate(0deg)";
+        boldToggle.style.fontWeight = 'normal';
 
     } else {
         textToggle.style.display = "block";
         imageToggle.style.transform = "rotate(90deg)";
+        boldToggle.style.fontWeight = 'bold';
     }
 }
