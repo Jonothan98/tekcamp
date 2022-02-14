9b506b9ad6f38de3a975933691b58e45496b0bf8
Jonothan <jonothan.x.castillo@gmail.com>
2022-02-14 10:51:01 -0600

    fixed issue with profile.js not working upon refresh

--- react_lab/react-app/src/componets/Profile.js
+++ react_lab/react-app/src/componets/Profile.js
@@ -1,4 +1,3 @@
-import { getSuggestedQuery } from "@testing-library/react";
 import axios from "axios";
 import react, { useEffect, useState } from "react";
 import './style.css';
@@ -6,49 +5,36 @@ import './style.css';
 const API_Key = process.env.REACT_APP_API_KEY;
 
 export default function Profile(){
-    const [userData, setUserData] = useState([]);
+    const [userData, setUserData] = useState({});
+    const [userBday, setUserBday] = useState("");
+    const [userLocation, setUserLocation] = useState({});
 
     useEffect(() =>{
-        getUser();
+        getUser()
+
     },[])
 
     const getUser = async () => {
         const headers = {"app-id": API_Key} 
         const response = await axios.get('https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca', {headers})
         setUserData(response.data)
-        console.log(userData)
+        setUserLocation(response.data.location)
+        setUserBday(response.data.dateOfBirth)
+
+        //console.log(response.data)
     }
+
 
     return(
         <div className="profileContainer">
             <img src={userData.picture} alt="Profile picture"/>
             <h2>{userData.firstName} {userData.lastName}</h2>
             <h2>Gender: {userData.gender}</h2>
-            <h2>BirthDate: {userData.dateOfBirth.substring(0,10)}</h2>
-            <h2>City: {userData.location.city}</h2>
-            <h2>State: {userData.location.state}</h2>
-            <h2>Country: {userData.location.country}</h2>
+            <h2>BirthDate: {userBday.substring(0,10)}</h2>
+            <h2>City: {userLocation.city}</h2>
+            <h2>State: {userLocation.state}</h2>
+            <h2>Country: {userLocation.country}</h2>
         </div>
     );
+
 }
-
-{/* <div className="profileContainer">
-            <img src="/" alt="avatar"/>
-            <h2>Jonothan Castillo</h2>
-            <h2>gender: Male</h2>
-            <h2>July 12, 1998</h2>
-            <h2>1234 Sesame street</h2>
-        </div> */}
-
-{/* <div>
-            {userData.map((userInfo) => (
-                <div className="profileContainer" key={userInfo.id}>
-                    {console.log(userInfo)}
-                    <img src="/" alt="avatar"/>
-                    <h2>{userInfo.firstName}</h2>
-                    <h2>gender: Male</h2>
-                    <h2>July 12, 1998</h2>
-                    <h2>1234 Sesame street</h2>
-                </div>
-            ))}
-        </div> */}
