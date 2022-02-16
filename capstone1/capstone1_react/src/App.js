5ba03e493391a604deb29578a7b2515cf3be0644
Jonothan <jonothan.x.castillo@gmail.com>
2022-02-16 16:07:18 -0600

    created capstone branch, react project, JSON file, and rough draft productPage.js

---
+++ capstone1/capstone1_react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProductPage from './Componets/ProductPage'
+
+
+
+
+function App() {
+  return (
+    <div>
+      <ProductPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ capstone1/capstone1_react/src/Componets/ProductPage.js
@@ -0,0 +1,32 @@
+import React,{ useState, useEffect } from "react";
+import products from '../dataBase.json'
+
+function ProductPage(){
+
+    const [productData, setData] = useState([]);
+
+    useEffect(() => {
+        setData(products.products);
+        console.log(productData);
+    }, []);
+    
+    return(
+        <div>
+            {productData.map((data) => {
+                return(
+                <div key={data.serialNumber}>
+
+                    <h1>{data.name}</h1>
+                    <h1>{data.serialNumber}</h1>
+                    <h1>{data.price}</h1>
+                    <h1>{data.manufacturer}</h1>
+                    <h1>{data.tags}</h1>
+                    <h1>{data.quantity}</h1>
+                </div>
+                )
+            })}
+        </div>
+    );
+}
+
+export default ProductPage;
