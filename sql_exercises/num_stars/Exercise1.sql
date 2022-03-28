1aeea991d8f5dce8f2dcb344fb179a5174544a51
Jonothan <jonothan.x.castillo@gmail.com>
2022-03-28 17:50:03 -0500

    Completed Exercise 1 Num Stars

---
+++ sql_exercises/num_stars/Exercise1.sql
@@ -0,0 +1,12 @@
+--  CREATE TABLE rating(
+--  	rating_id INT PRIMARY KEY AUTO_INCREMENT,
+--      number_of_stars INT UNIQUE,
+--      stars_description VARCHAR(100)
+--  );
+INSERT INTO rating(number_of_stars, stars_description)
+VALUES(1,'The movie was pretty bad'),
+(2,'The movie was pretty bad but not as bad'),
+(3,'The movie was bad'),
+(4,'The movie is getting into meh territory'),
+(5,'The movie is meh');
+SELECT * FROM rating;
